0501b280da7af4f5b2200c739696a2847b428a65
VC76884070 <vaniacarrasco76884070@gmail.com>
2026-08-26 13:45:32 -0400

    cotizaciones intento 5

--- jefe_taller/js/cotizaciones.js
+++ jefe_taller/js/cotizaciones.js
@@ -2317,7 +2317,7 @@ function renderOrdenesCotizacionCliente() {
             </div>
         </div>`;
     }).join('');
-}
+}s
 
 function renderHistorialCotizaciones() {
     const container = document.getElementById('historialCotizacionesContainer');
